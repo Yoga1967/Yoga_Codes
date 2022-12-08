09ad1f67035efd3f7f7f2a50db47152d5b3aab7f
Yoga1967 <adrianuseka67@gmail.com>
2022-12-09 04:31:29 +0700

    commit update sp ie
    commit update sp ie

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@ $('.year-number').on('click', function () {
     if ($(this).siblings(".point-line")) {
         $(".point-line").removeClass('active');
         $(this).siblings(".point-line").toggleClass("active");
+
         var index = $(this).parent().index();
-        var index1 = index + 2;
-        console.log(index);
+        var index1 = index + 4;
+        console.log(index1);
         $(".years-image").removeClass("active");
         $(".content-second").children().eq(index1).toggleClass("active");
-        
+
         $(".date").removeClass("date-active");
         $(".date").eq(index).toggleClass("date-active");
-    } 
+    }
 });
 
 
@@ -23,3 +24,36 @@ if ($('.point-line').hasClass(".active")) {
         $('#square').removeClass('highlight');
     }, 10000);
 }
+
+$('#dropdown').on('change', function (e) {
+    $('.tab-pane').removeClass('active in')
+    $('#' + $(e.currentTarget).val()).addClass("active in");
+})
+if ($(window).width() < 769) {
+    $('.presale-box').slick({
+        autoplay: true,
+        dots: true,
+        autoplaySpeed: 3000,
+        speed: 250
+    });
+}
+$('.burger-menu-sp a').on('click', function () {
+    $('.menu').removeClass('hide-menu');
+    $('.menu').addClass('show-menu');
+});
+$('.burger-menu-sp-close a').on('click', function () {
+    $('.menu').addClass('hide-menu');
+    $('.menu').removeClass('show-menu');
+});
+
+$('.read-more-button').on('click', function () {
+
+    var btnText = document.getElementById("myBtn");
+    if ($('.read-more').hasClass("hide-sp-only")) {
+        $('.read-more').removeClass("hide-sp-only");
+        btnText.innerHTML = "Read less";
+    } else {
+        $('.read-more').addClass("hide-sp-only");
+        btnText.innerHTML = "Read more";
+    }
+});
